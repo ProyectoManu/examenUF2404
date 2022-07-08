06b48bb934a453754d21bdf4bddf57137b15766c
Manuel López <Manuel Lpez@DESKTOP-EBN4BIK>
2022-07-08 14:23:12 +0200

    Ejercicio 1 Examen uf2404

---
+++ examenUF2404_apellidos_nombre/src/Modelo/Hora.java
@@ -0,0 +1,74 @@
+package Modelo;
+
+public class Hora {
+	
+	private int horas;
+	private int minutos;
+	private int segundos;
+	
+	public Hora() {
+		super();
+		this.horas=0;
+		this.minutos=0;
+		this.segundos=0;
+	}
+
+	public Hora(int horas, int minutos, int segundos) {
+		super();
+		this.horas = horas;
+		this.minutos = minutos;
+		this.segundos = segundos;
+	}
+	
+	public Hora(Hora p) {
+		super();
+		this.horas = p.horas;
+		this.minutos = p.minutos;
+		this.segundos = p.segundos;
+
+}
+	static private valida(int hora, int minutos, int segundos) {
+		
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public String toString() {
+		return String.format("La hora es %2f", "los minutos son %2F", "y los segundos son %2f");
+	}
+
+	public void asegundos() {
+		return $timeofdayseconds = $epochseconds - $midnight; 
+		
+}
+	public void desegundos(int segundos) {
+		String horas ="23";
+        String minutos ="00";
+       
+        
+        int horasegundos = horasInt /60;
+        int segundoMinutos = minutosInt / 60;
+        in tottalSegundo = horasegundos + segundosMinuto;
+        
+       
+		
+	}
+
+	public void segundos_entre_horas(int num) {
+	}
+	}
+
